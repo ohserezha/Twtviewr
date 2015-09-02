4389747e658cc9854a7e2173b8d9a4a732cdfdef
Sergey G <gssreg@gmail.com>
2015-09-02 23:30:57 +0300

    Initial Commit

---
+++ Twtviewr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twtviewr
+//
+//  Created by ezkeemo on 9/2/15.
+//  Copyright (c) 2015 ezkeemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twtviewr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twtviewr
+//
+//  Created by ezkeemo on 9/2/15.
+//  Copyright (c) 2015 ezkeemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
